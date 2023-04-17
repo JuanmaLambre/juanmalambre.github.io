a3382bc3727da2af032e02be7bd7106c2004be0f
Juan Lambre <juanmlambre@gmail.com>
2023-04-17 11:39:35 -0300

    Update

--- microlab/update.sh
+++ microlab/update.sh
@@ -8,3 +8,7 @@ cp $ORIGIN/dist/app.bundle.js $ORIGIN/dist/app.css $ORIGIN/dist/index.html $DEST
 cp $ORIGIN/public/maps/* $DESTINATION/public/maps
 cp $ORIGIN/public/models/* $DESTINATION/public/models
 
+cd $DESTINATION
+git add ./**
+git commit -am Update
+gitpush
