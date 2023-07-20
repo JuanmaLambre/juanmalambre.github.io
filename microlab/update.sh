6e94c7271d0956b0aa15c80e8f9698afdb20328b
Juan Lambre <juanmlambre@gmail.com>
2023-07-20 12:02:05 -0300

    Update

--- microlab/update.sh
+++ microlab/update.sh
@@ -1,10 +1,13 @@
+REPODIR=/Users/juanme/workplace/juanmalambre.github.io
 ORIGIN=/Users/juanme/workplace/CITEP/labVRMicroUBA
-DESTINATION=/Users/juanme/workplace/juanmalambre.github.io/microlab
+DESTINATION=$REPODIR/microlab
 
 cd $ORIGIN
 npm run build
 
 cp $ORIGIN/dist/app.bundle.js $ORIGIN/dist/app.css $ORIGIN/dist/index.html $DESTINATION
+cp $ORIGIN/public/fonts/* $REPODIR/public/
+cp $ORIGIN/dist/*.mp3 $REPODIR/public/
 cp -r $ORIGIN/public/** $DESTINATION/../public/
 
 cd $DESTINATION
