27bd1ced41a89a3e9badcd2352c1a156b6bfe70e
Juan Lambre <juanmlambre@gmail.com>
2024-01-17 19:09:50 -0300

    Redesign

--- cv/scripts/circle-pixels-cloud.js
+++ cv/scripts/circle-pixels-cloud.js
@@ -4,10 +4,10 @@ const NOISE_THRESHOLD = 0.5; // -1 to 1
 const SPEED = 1 / 2000;
 
 new p5((s) => {
-  const { PI, cos } = Math;
+  const { PI, cos, sin } = Math;
 
   s.setup = function () {
-    const canvas = document.getElementById("header-bg");
+    const canvas = document.getElementById("circle-cloud");
     const { clientWidth: width, clientHeight: height } = canvas;
     s.createCanvas(width, height, "2d", canvas);
 
@@ -18,13 +18,15 @@ new p5((s) => {
   s.draw = function () {
     const { width, height, map, noise } = s;
     const t = s.millis() * SPEED;
-    const circleRadius = GRID_DISCRETIZATION * (1 + Math.sin((t / 2) * 2 * PI) / 2);
 
     s.background(0);
     s.noStroke();
 
     for (let x = 0; x < width; x += GRID_DISCRETIZATION) {
       for (let y = 0; y < height; y += GRID_DISCRETIZATION) {
+        // const circleRadius = GRID_DISCRETIZATION * (1 + Math.sin((t / 2) * 2 * PI) / 2);
+        const circleRadius = GRID_DISCRETIZATION * (1 + sin(PI * (t - x / width)) / 2);
+
         const rNoise = noise(cos(t), x / NOISE_ZOOM, y / NOISE_ZOOM / 1000);
         if (rNoise > NOISE_THRESHOLD) {
           const alpha = map(rNoise, NOISE_THRESHOLD, 1, 0, 230);
