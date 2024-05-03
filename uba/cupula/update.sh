ff94f5462f4c9b975a2f9fc58911c74380630d6d
Juan Lambre <juanmlambre@gmail.com>
2024-05-03 12:24:23 -0300

    Update cupula

--- uba/cupula/update.sh
+++ uba/cupula/update.sh
@@ -3,14 +3,17 @@
 ORIGIN_DIR="/Users/juanme/workplace/UBATIC/astro-cupula"
 DESTINATION_DIR="/Users/juanme/workplace/juanmalambre.github.io/uba/cupula"
 
+rm -rf $DESTINATION_DIR/astro
+
 cd $ORIGIN_DIR
 npm run build
 cp -r dist/** $DESTINATION_DIR
 
 cd $DESTINATION_DIR
 mv _astro astro
-sed -i -e 's/_astro/.\/astro/g' index.html
+sed -i -e 's:_astro:uba/cupula/astro:g' index.html
+sed -i -e "s|<style>.*</style>|<style>$(cat $(ls astro/index.*.css))</style>|g" index.html 
 
 git add . -A
 git commit -am "Update cupula"
-#git push
+git push
