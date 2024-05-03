e63690f5f5a56369f099341c9f94248419436b3a
Juan Lambre <juanmlambre@gmail.com>
2024-05-03 10:52:57 -0300

    Update cupula

--- uba/cupula/update.sh
+++ uba/cupula/update.sh
@@ -6,9 +6,11 @@ DESTINATION_DIR="/Users/juanme/workplace/juanmalambre.github.io/uba/cupula"
 cd $ORIGIN_DIR
 npm run build
 cp -r dist/** $DESTINATION_DIR
-cp -r public $DESTINATION_DIR
 
 cd $DESTINATION_DIR
+mv _astro astro
+sed -i -e 's/_astro/astro/g' index.html
+
 git add . -A
-git commit -am "Update resorte"
+git commit -am "Update cupula"
 git push
