051a43d55fcb8bf3521dd09aa64665a9146a3130
Juan Lambre <juanmlambre@gmail.com>
2024-05-03 11:07:12 -0300

    Update cupula

--- uba/cupula/update.sh
+++ uba/cupula/update.sh
@@ -9,8 +9,8 @@ cp -r dist/** $DESTINATION_DIR
 
 cd $DESTINATION_DIR
 mv _astro astro
-sed -i -e 's/_astro/uba\/cupula\/astro/g' index.html
+sed -i -e 's/_astro/.\/astro/g' index.html
 
 git add . -A
 git commit -am "Update cupula"
-git push
+#git push
