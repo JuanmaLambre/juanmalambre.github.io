1ea201abcd029a29f37e4e55259e78aa4b3c7b0e
Juan Lambre <juanmlambre@gmail.com>
2024-08-28 23:22:37 -0300

    Update raspadita-v2

---
+++ raspadita-v2/update.sh
@@ -0,0 +1,12 @@
+DEST=$(dirname $(pwd)'/'$0)
+
+cd ~/workplace/raspadita-ypf-v2
+npm run build
+
+cp -r dist/bundle.js dist/index.html $DEST
+
+cd $DEST
+git add . -A
+git commit -am "Update raspadita-v2"
+git push
+
